df14fa727e4dc6f79a6d3d7b73fd7b5d7636abf8
UmamaSayed <umamasayed2910@gmail.comclear>
2024-03-21 00:56:29 +0530

    list items and backimg Added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,9 +31,9 @@ const Header = () => {
           {/* Side menu items */}
           <ul className="menu-list">
             <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Contact</li>
+            <li>Campaign Ideas</li>
+            <li>Metric Measures</li>
+            <li>Create Camapaign</li>
           </ul>
         </div>
         <div className="content">
