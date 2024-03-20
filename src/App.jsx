3fbd34cbf3b84d0a2b7a551fc49472451f40b516
UmamaSayed <umamasayed2910@gmail.comclear>
2024-03-21 00:01:39 +0530

    Header Section

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/header/Header';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     {/* <HeroSection /> */}
+     {/* <SlidingBar /> */}
+     {/* <Benefits /> */}
+     {/* <Metrics /> */}
+     {/* <Campaign /> */}
+     {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <>
+      <header className="header">
+        <a href="#" className="logo">
+          Activatr
+        </a>
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
+        </div>
+        <div className={`side-menu ${menuOpen ? "open" : ""}`}>
+          {/* Close button for sidebar */}
+          {menuOpen && (
+            <div className="close-button" onClick={closeMenu}>
+              &times;
+            </div>
+          )}
+          {/* Side menu items */}
+          <ul className="menu-list">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="content">
+          <div className="right"></div>
+          <div className="left">
+            <div className="title">
+              <h1>
+                Unlock Growth with ACTIVATR: Optimize Strategies, Empower
+                Businesses.
+              </h1>
+              <p className="description">
+                Experience precision marketing through geolocation targeting,
+                AI-driven insights, and secure offline storage for enhanced data
+                accessibility and security.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
